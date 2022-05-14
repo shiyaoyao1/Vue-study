b5abbec45c86680af801b9d8dec54bb562cf2309
shiyaoyao1 <1321591813@qq.com>
2022-05-14 23:35:28 +0800

    2022/5/14 night

---
+++ 19_单文件组件/vue_test/原src/components/SchoolName.vue
@@ -0,0 +1,34 @@
+<template>
+<!-- 组件的结构 -->
+<div class="demo">
+    <h2>校名:{{name}}</h2>
+    <h2>地区:{{address}}</h2> 
+    <button @click='showName'>点我提示地区</button>
+</div>
+</template>
+
+<script>
+//交互相关代码(数据,方法等)
+    export default {
+        name:'SchoolName',
+        data(){
+            return {
+                name:'CQUPT',
+                address:'重庆'
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.address);
+            }
+        },
+    }
+</script>
+
+<style>
+/* 组件的样式 */
+.demo{
+    background-color: rgb(255, 162, 108);
+}
+
+</style>
